f3ece37413b3b513ea179eea8528416f516b5cb1
Michael März <michael0maerz@gmail.com>
2015-10-13 14:58:24 +0200

    Adaboost_class finished, using the Adaboost.R2 for regression,

--- boosted_regression.py
+++ boosted_regression.py
@@ -1,9 +1,9 @@
-from sklearn.metrics import mean_squared_error
 from Custom_Statistics import *
 from sklearn import tree
 import sys
 
-class AdaboostRegression():
+
+class AdaboostRegression:
     """
     Class for boosted regressions, using tree based methods
 
@@ -30,21 +30,111 @@ class AdaboostRegression():
 
     """
 
-    def ___init__(self, n_estimators,
-                  loss_fkt='exponential',
-                  estimator_list=None,
-                  estimator_error=None):
+    def __init__(self, n_estimators=2,
+                 loss_fkt='linear',
+                 estimator_list=None,
+                 estimator_error=None,
+                 tree_parameters=None
+                 ):
 
         self.n_estimators = n_estimators
         self.loss_fkt = loss_fkt
-
+        self.tree_parameters = tree_parameters
         self.estimator_list = estimator_list
+        self.estimator_weights = []
+
+        if tree_parameters is None:
+            self.tree_parameters = {'max_depth': 20}
         if self.estimator_list is None:
             self.estimator_list = []
 
         self.estimator_error = estimator_error
         if self.estimator_error is None:
             self.estimator_error = []
+
+    @staticmethod
+    def choose_data(features, targets, weights):
+        """
+        Chooses Data by bootstrapping
+
+        :param features: list of features
+        :param targets:  list of targets
+        :param weights:  normalized prob. dist.
+
+        :return: bootstrapped features, targets and weights
+        """
+
+        # Select the Index by weight using np.choice
+        index_arr = np.arange(0, len(features))
+        random_index_arr = np.random.choice(index_arr, size=int(len(features)), replace=True, p=weights)
+
+        # Get the randomized array
+        x = features[random_index_arr]
+        y = targets[random_index_arr]
+
+        return x, y
+
+    def build_tree(self, x, y):
+        """
+        Builds a single tree as an weak learner
+
+        :param x: features
+        :param y: targets
+
+        :return:  estimator
+        """
+        clf = tree.DecisionTreeRegressor(**self.tree_parameters)
+        clf.fit(x, y)
+
+        return clf
+
+    def update_weights(self, features, targets, weights, estimator):
+        """
+        We will predict on the original Data, calculate Delta and update the weights
+
+        If the loss fkt. is invalid we always use linear-loss
+        For more info about Adaboost.R2:
+        http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.100.4560&rep=rep1&type=pdf
+
+        :param features:
+        :param targets:
+        :param weights:     normalized prob. dist
+        :param estimator:   a weak learner: decision tree regressor
+        :return:
+        """
+
+        # Make a prediction
+        predicted = estimator.predict(features)
+
+        # Calculate Delta
+        delta = abs(predicted - targets)
+
+        # As element-wise loss we use:
+        sup = delta.max()
+        if sup != 0:
+            element_loss = delta / delta.max()
+        else:
+            print('delta_max = 0, aborting')
+            return 0
+
+        if self.loss_fkt == 'exponential':
+            element_loss = 1. - np.exp(- element_loss)
+        if self.loss_fkt == 'square':
+            element_loss **= 2
+
+        # Average Loss
+        average_loss = (element_loss * weights).sum()
+
+        # Calculate the Confidence
+        betha = average_loss / (1 - average_loss)
+
+        # Update weights
+        weights = weights * np.power(betha, (1. - element_loss))
+
+        # Estimator weights
+        estimator_weight = 1. / np.log(betha)
+
+        return weights, estimator_weight
 
     def fit(self, features, targets, weights=None):
         """
@@ -57,46 +147,65 @@ class AdaboostRegression():
         """
 
         # Check Data
-        if len(features) != len(targets):
-            raise ValueError('feature length:' + str(len(features)) +
-                             'doesn\'t match target length: ' + str(len(targets)))
+        if False: #len(features) != len(targets):
+            raise ValueError('feature length: ' + str(len(features)) +
+                             ' doesn\'t match target length: ' + str(len(targets)))
 
         # Fill the weights
-        if weights == None:
+        if weights is None:
             '''
-            Each element same, nomalized weight,
+            Each element same, normalized weight,
                 Fun Fact: np.empty + arr.fill is super fast, check:
                 http://stackoverflow.com/questions/5891410/numpy-array-initialization-fill-with-identical-values
             '''
             weights = np.empty(len(targets))
             weights.fill(1/len(targets))
 
-
-
         # Check weights
-        if np.sum(weights) != 1.:
+        if round(np.sum(weights), 10) != 1.:
             raise ValueError('weights not normalized! ' + str(np.sum(weights)))
 
         # Assume all is fine, start the iteration
         for boost_step in range(self.n_estimators):
-            # waste computing power for loading bar
-            step = self.n_estimators/100
-            if boost_step%step == 0:
-                print('\rTraining: %s (%d%%)' % ("|"*(int(boost_step/(3*step))), boost_step/step), end="")
-                sys.stdout.flush()
-
             # Bootstrap/choose the training data
-            X, Y = choose_data(features, targets, weights)
+            bootstrapped_features, bootstrapped_targets = self.choose_data(features, targets, weights)
 
             # Build a tree
-            estimator = build_tree(X, Y)
+            estimator = self.build_tree(bootstrapped_features, bootstrapped_targets)
 
             # Save the estimator
             self.estimator_list.append(estimator)
 
-            #Get weights, and update them Error is calculate from the full sample
-            weights, error = update_weights(X, Y, weights, estimator)
+            # Get weights, and update them Error is calculate from the full sample
+            weights, estimator_error = self.update_weights(features, targets, weights, estimator)
+            self.estimator_weights.append(abs(estimator_error))
+            # Normalize weights
+            weights = normalize(weights)
+
+            # waste computing power for loading bar
+            step = self.n_estimators/100
+            print('\rTraining: %s (%d%%)' % ("|"*(int(boost_step/(3*step))), boost_step/step), end="")
+            sys.stdout.flush()
 
         # Now we have a list of n_estimators weak learners, we will use them to build a strong learner
-        return self.estimator_list, self.estimator_error
+        return self.estimator_list, self.estimator_weights
+
+    @staticmethod
+    def predict(features, est_list, weight):
+        """
+        makes the prediction with the estimators using skikit_learn decision_trees
+
+        :param features: validation_features
+        :param est_list: list of estimators
+        :param weight:   weight of the estimators
+        :return: prediction_vector
+        """
+        prediction = np.empty(len(features))
+        weight = normalize(weight)
+        for x in range(len(est_list)):
+            prediction += est_list[x].predict(features)*(weight[x])
+            print(weight[x]/np.sum(weight))
+        return prediction
+
+
 
